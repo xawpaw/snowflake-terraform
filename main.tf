4d452f3bdb4ddfdb84a213b8a9dc556b5561945a
Michael Mendy <montana@linux.com>
2024-05-24 15:31:30 -0700

    completed `main.tf`
    Added enhancements to the Snowflake Terraform configuration by granting the `MY_ROLE` to `MY_USER`, ensuring the user has the necessary role for Snowflake operations. It also grants the role usage permissions on the `MY_DATABASE` and `MY_SCHEMA`, allowing access and utilization of these resources. A new table `MY_TABLE` is created within the schema, structuring data storage with defined columns. Finally, the role is granted select permissions on the table, enabling it to query and retrieve data, thereby ensuring robust access management and data handling capabilities within Snowflake.
    
    Signed off by: Michael Mendy <montana@linux.com>

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ provider "snowflake" {
 }
 
 resource "snowflake_warehouse" "my_warehouse" {
-  name            = "MY_WAREHOUSE"
-  size            = "XSMALL"
-  auto_suspend    = 60
-  auto_resume     = true
+  name                = "MY_WAREHOUSE"
+  size                = "XSMALL"
+  auto_suspend        = 60
+  auto_resume         = true
   initially_suspended = true
 }
 
@@ -18,6 +18,59 @@ resource "snowflake_database" "my_database" {
 }
 
 resource "snowflake_schema" "my_schema" {
-  name      = "MY_SCHEMA"
+  name     = "MY_SCHEMA"
+  database = snowflake_database.my_database.name
+}
+
+resource "snowflake_role" "my_role" {
+  name = "MY_ROLE"
+}
+
+resource "snowflake_user" "my_user" {
+  name       = "MY_USER"
+  login_name = "my_user"
+  password   = "StrongPassword123!"
+  default_warehouse = snowflake_warehouse.my_warehouse.name
+  default_role      = snowflake_role.my_role.name
+}
+
+resource "snowflake_role_grants" "my_user_role_grant" {
+  role_name = snowflake_role.my_role.name
+  users     = [snowflake_user.my_user.name]
+}
+
+resource "snowflake_database_grant" "my_database_grant" {
+  database_name = snowflake_database.my_database.name
+  roles         = [snowflake_role.my_role.name]
+  privilege     = "USAGE"
+}
+
+resource "snowflake_schema_grant" "my_schema_grant" {
+  schema_name   = snowflake_schema.my_schema.name
+  database_name = snowflake_database.my_database.name
+  roles         = [snowflake_role.my_role.name]
+  privilege     = "USAGE"
+}
+
+resource "snowflake_table" "my_table" {
+  name      = "MY_TABLE"
   database  = snowflake_database.my_database.name
+  schema    = snowflake_schema.my_schema.name
+  comment   = "This is my table"
+  column {
+    name = "ID"
+    type = "NUMBER(38,0)"
+  }
+  column {
+    name = "NAME"
+    type = "STRING"
+  }
+}
+
+resource "snowflake_table_grant" "my_table_grant" {
+  table_name   = snowflake_table.my_table.name
+  schema_name  = snowflake_schema.my_schema.name
+  database_name = snowflake_database.my_database.name
+  roles         = [snowflake_role.my_role.name]
+  privilege     = "SELECT"
 }
